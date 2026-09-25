bb560843b36b64aff9a215717805593a1e767b40
min <minhchau2441@gmai.com>
2026-09-25 08:55:18 +0700

    practice 5 + 6

--- app/src/main/java/vn/edu/usth/weatherapp/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weatherapp/ForecastFragment.java
@@ -17,27 +17,7 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // 1. Create icon
-        TextView day = new TextView(getContext());
-        day.setText("Thursday");
-
-        ImageView img = new ImageView(getContext());
-        img.setImageResource(R.drawable.maincloudy);
-        img.setBackgroundColor(Color.parseColor("#aac7f0"));
-
-        // 2. Create background color
-        // Way 1
-        View v = inflater.inflate(R.layout.fragment_forecast, container, false);
-        v.setBackgroundColor(Color.parseColor("#aac7f0"));
-
-        // Way 2 : dynamic
-        LinearLayout view = new LinearLayout(getContext());
-        view.setOrientation(LinearLayout.VERTICAL);
-
-        view.addView(day);
-        view.addView(img);
-
-        // return inflater.inflate(R.layout.fragment_forecast, container, false);
-        return view;
+        // Nạp và trả về View từ file XML fragment_forecast.xml
+        return inflater.inflate(R.layout.fragment_forecast, container, false);
     }
 }
